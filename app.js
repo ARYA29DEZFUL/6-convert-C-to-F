d2a690a88561baf142d61cc7c481f85c6ec7e18d
Aria <ARYAir.121212@gmail.com>
2025-05-12 17:35:08 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let $ = document
+
+let CONVERTButton = $.getElementById("CONVERT")
+let RESETButton = $.getElementById("RESET")
+let CHANGEButton = $.getElementById("CHANGE")
+
+let textToChange = $.getElementById("textToChange")
+let inputValue = $.getElementById("inputValue")
+
+let flag = false
+
+CHANGEButton.addEventListener("click", () => {
+    if (flag) {
+        textToChange.innerHTML = "℃ to ℉"
+        inputValue.placeholder = "℃"
+        flag = false
+    } else {
+        textToChange.innerHTML = "℉ to ℃"
+        inputValue.placeholder = "℉"
+        flag = true
+    }
+})
